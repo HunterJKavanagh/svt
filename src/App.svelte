bc07eb202cdced27bbe03abc198f43201275705a
Hunter Kavanagh <hunter.kavanagh@protonmail.com>
2023-06-15 13:08:05 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<main class="">
+  <header class="bg-blue-800 text-white flex justify-between p-2 sticky top-0">
+    <h1 class="text-xl">Test Project</h1>
+  </header>
+</main>
